c4d692213a6652a79b39a5fd1294a25622b0ac0c
Lenear <amagovelenear@gmail.com>
2022-10-24 09:38:22 +0300

    Update email.php

--- application/config/email.php
+++ application/config/email.php
@@ -5,7 +5,7 @@
   'smtp_host' => 'smtp.gmail.com',
   'smtp_crypto' => 'tls',
   'smtp_port' => 587,//25,587,465 or 2525
-  'smtp_user' => '', // change it to yours
+  'smtp_user' => '', // change it to your gmail
   'smtp_pass' => '', // change it to yours
   'mailtype' => 'html',
   'charset' => 'iso-8859-1',
